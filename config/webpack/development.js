0b36b7514efa23fcc1173e1ea4ef0d285d28df09
Bruna <brunagenz92@gmail.com>
2019-11-06 12:11:57 -0400

    create specific directories for each stylesheet category; change nav bar position to fixed

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+stats: {
+    errors: true;
+    errorDetails: true;
+    colors: true;
+    optimizationBailout: true;
+   }
