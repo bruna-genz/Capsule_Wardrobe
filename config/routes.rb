36ccc6859c5e13d662bb975f121c775f15b2e046
Bruna <brunagenz92@gmail.com>
2019-11-06 13:58:50 -0400

    Change sign up page's route. Change path of sign up link on hope page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
