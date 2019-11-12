3828acf669c32cb9361cd379f4a2aa73e7f8a39a
Bruna <brunagenz92@gmail.com>
2019-11-12 18:50:43 -0400

    install bootstrap via webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap-sprockets")
-require("jquery3")
+//= require jquery3
+//= require popper
+//= require bootstrap
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +17,10 @@ require("jquery3")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+import './stylesheets/application'
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
