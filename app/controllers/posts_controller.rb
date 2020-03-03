ed25df9c1c2631fcc6293ffb2802d3a88bbfc576
Bruna <brunagenz92@gmail.com>
2020-03-03 16:26:05 -0400

    Add categories to clothes
    create select tag on new clothes form; create default categories on seeds.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
-class PostsController < ApplicationController
-  
+class PostsController < ApplicationController  
   def index
   end
 
   def new
     @post = Post.new
+    @categories = Category.all.map{ |cat| [cat.name, cat.id] }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_202409) do
+ActiveRecord::Schema.define(version: 2020_03_03_191826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_12_01_202409) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
