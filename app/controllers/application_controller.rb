4f5ae0050790c675a2229941df43337e6662af52
Bruna <brunagenz92@gmail.com>
2020-03-03 18:56:25 -0400

    Fix styles of posts index
    add attached images as background; edit sidebar to iterate over categories; put badge with count of items for each category.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     posts_path
   end
+
+  def get_categories 
+    @categories = Category.all
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController  
   def index
-    @posts = Post.all
+    @posts = current_user.posts
   end
 
   def new
