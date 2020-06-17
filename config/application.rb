74efeca364d6360d5801953cf4fb03d0237b170a
Bruna <brunagenz92@gmail.com>
2020-06-17 15:24:26 -0300

    upload logo image

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require 'rails/all'
 
 require 'sprockets/railtie'
 
+require "active_storage/engine"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,7 +15,7 @@ module CapsuleWardrope
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_controller.include_all_helpers = false
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
